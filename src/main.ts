2acced179e9a5f1ffefc53f1373b68ea04efb6f5
kaito3desuyo <admin@seapolis.me>
2020-04-05 08:13:08 +0000

    バリデーション設定の追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { customValidationPipe } from './core/pipes/custom-validation.pipe';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
 
     const swagger = SwaggerModule.createDocument(app, swaggerOptions);
     SwaggerModule.setup('doc', app, swagger);
+
+    app.useGlobalPipes(customValidationPipe);
 
     await app.listen(3000);
 }
