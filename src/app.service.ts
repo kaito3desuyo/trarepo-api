89f3c43d76f05246e4c28d934d700aa4bcc4e469
kaito3desuyo <admin@seapolis.me>
2020-04-05 06:33:43 +0000

    devcontainer周りの設定を追加

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Nest.js World!';
-  }
+    getHello(): string {
+        return 'Hello Nest.js World!';
+    }
 }
