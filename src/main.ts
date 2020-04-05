e9947524a9857934334d5696a8cb16df0fb4e6b6
kaito3desuyo <admin@seapolis.me>
2020-04-05 07:59:53 +0000

    Swaggerの設定を追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    const swaggerOptions = new DocumentBuilder()
+        .setTitle('Trarepo API')
+        .setDescription('全国公共交通機関データベース')
+        .build();
+
+    const swagger = SwaggerModule.createDocument(app, swaggerOptions);
+    SwaggerModule.setup('doc', app, swagger);
+
     await app.listen(3000);
 }
 bootstrap();
